9dc68e310ac523506cdda43d49b79da2a4bf2fd3
Efjay <efjayengelbrecht@gmail.com>
2025-07-20 10:59:56 +0200

    playRound errors repaired with scores. playGame to be updated with 5 rounds and scores to be added to determine winner.

--- script.js
+++ script.js
@@ -1,51 +1,4 @@
-
- /*
-  
-  function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let tieScore = 0;
-
-  function playRound(playerSelection, computerSelection) {
-    // Your playRound logic here
-    // Example:
-    if (playerSelection === computerSelection) {
-      tieScore++;
-      return "It's a tie!";
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      playerScore++;
-      return "You win!";
-    } else {
-      computerScore++;
-      return "You lose!";
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    // Get player and computer choices (replace with your actual implementation)
-    const playerSelection = prompt("Enter your choice: rock, paper, or scissors");
-    const computerChoices = ["rock", "paper", "scissors"];
-    const computerSelection =
-      computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    console.log(playRound(playerSelection, computerSelection));
-  }
-
-  console.log(`Player: ${playerScore}, Computer: ${computerScore}, Ties: ${tieScore}`);
-  // Determine the winner based on final scores
-}
-
-playGame();
-
-*/
-
-//-----------------Code Above Copied from Google as reference----------------//
-  
- let choices = ["rock", "paper", "scissor"];
+let choices = ["rock", "paper", "scissor"];
  
 
 
@@ -56,47 +9,47 @@ playGame();
   let randomNumber = Math.floor(Math.random() * 3)
 return choices[randomNumber];
 };
-console.log(getComputerChoice())
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
 
 function getHumanChoice() {choices};
 let humanSelection = getHumanChoice();
 
 humanSelection = window.prompt("Enter Selection");
-//humanSelection = getHumanChoice.toLowerCase();
-
+console.log(humanSelection);
+//###################//
+  let humanScore = 0;
+  let computerScore = 0;
+  let tieScore = 0;
+//###################//
 
 function playRound(humanSelection, computerSelection) {
 
 if (humanSelection === computerSelection) { 
-   return "It's a Tie!";
+   return `It's a Tie! No Points`;
   } else if (
  (humanSelection === 'rock' && computerSelection === 'scissor') ||
  (humanSelection === 'scissor' && computerSelection === 'paper') ||
  (humanSelection === 'paper' && computerSelection === 'rock') 
   ) {
-    return `you win!, ${humanSelection}! beats ${computerSelection}`;
+    return `you win!, ${humanSelection} beats ${computerSelection} Player gets ${humanScore++} point!`;
   } else {
-    return `you lose!, ${computerSelection}! beats ${humanSelection}.`; 
+    return `you lose!, ${computerSelection} beats ${humanSelection} Computer gets ${computerScore++} point!`; 
   }
 }
-const computerSelection = getComputerChoice();
+
 playRound(humanSelection, computerSelection);
-
 console.log(playRound(humanSelection, computerSelection));
-
-
-
     
 /*#############*/
-
-const humanScore = 0;
-const computerScore = 0;
-const tieScore = 0;
+  
+function playGame(playRound)
+{
+  for (let i = 0; i < 5; i++) {
+        
+  };
+};
+ 
+console.log(playGame());
 
 /*#############*/
-
-function playGame()
-{
-
-} 
-
